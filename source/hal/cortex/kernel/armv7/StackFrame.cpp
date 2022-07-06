c1f254ca3cf38bec991a1a886a1d89066c27fd7f
Daan Timmer <daan.timmer@gmail.com>
2022-07-07 00:30:43 +0200

    added StackFrame implementation

--- source/hal/cortex/kernel/armv7/StackFrame.cpp
+++ source/hal/cortex/kernel/armv7/StackFrame.cpp
@@ -1,1 +1,19 @@
 #include "hal/cortex/kernel/armv7/StackFrame.hpp"
+#include <new>
+
+namespace hal::kernel
+{
+    void* InitializeStackFrame(void* const buffer, std::size_t size, void* obj)
+    {
+        const auto stackFramePtr = reinterpret_cast<StackFrame*>(static_cast<std::uint8_t*>(buffer) + size) - 1;
+
+        const auto stackFrame = new (stackFramePtr) StackFrame{};
+
+        stackFrame->exceptionStackFrame.r0 = obj;
+
+        stackFrame->exceptionStackFrame.lr = nullptr;
+        stackFrame->exceptionStackFrame.pc = nullptr;
+
+        return stackFrame;
+    }
+}
--- source/hal/cortex/kernel/armv7/StackFrame.hpp
+++ source/hal/cortex/kernel/armv7/StackFrame.hpp
@@ -1,37 +1,55 @@
 #ifndef ARMV7_STACKFRAME_HPP
 #define ARMV7_STACKFRAME_HPP
 
-#include <cstdint>
 #include "device.hpp"
+#include <cstdint>
 
 namespace hal::kernel
 {
-    using wr = void *;
+    using wr = void*;
 
     struct SoftwareStackFrame
     {
-        wr r4;
-        wr r5;
-        wr r6;
-        wr r7;
-        wr r8;
-        wr r9;
-        wr r10;
-        wr r11;
+        static constexpr std::uintptr_t exceptionReturnDefaultValue{0xFF'FF'FF'FD};
+
+#if __CORTEX_M == 4
+        void* r4{reinterpret_cast<void*>(0x44444444)};
+        void* r5{reinterpret_cast<void*>(0x55555555)};
+        void* r6{reinterpret_cast<void*>(0x66666666)};
+        void* r7{reinterpret_cast<void*>(0x77777777)};
+#endif
+
+        void* r8{reinterpret_cast<void*>(0x88888888)};
+        void* r9{reinterpret_cast<void*>(0x99999999)};
+        void* r10{reinterpret_cast<void*>(0xAAAAAAAA)};
+        void* r11{reinterpret_cast<void*>(0xBBBBBBBB)};
+
+#if __CORTEX_M != 4
+        void* r4{reinterpret_cast<void*>(0x44444444)};
+        void* r5{reinterpret_cast<void*>(0x55555555)};
+        void* r6{reinterpret_cast<void*>(0x66666666)};
+        void* r7{reinterpret_cast<void*>(0x77777777)};
+#endif
+
+#if __FPU_PRESENT == 1 && __FPU_USED == 1
+        void* reserved{0};
+        void* exceptionReturn{reinterpret_cast<void*>(exceptionReturnDefaultValue)};
+#endif
     };
 
     struct ExceptionStackFrame
     {
-        static constexpr std::uintptr_t defaultXpsr{0x01'00'00'00};
+        static constexpr std::uintptr_t xpsrDefaultValue{0x01'00'00'00};
 
-        wr r0;
-        wr r1;
-        wr r2;
-        wr r3;
-        wr r12;
-        wr lr;
-        wr pc;
-        wr xpsr;
+        void* r0{reinterpret_cast<void*>(0x00000000)};
+        void* r1{reinterpret_cast<void*>(0x11111111)};
+        void* r2{reinterpret_cast<void*>(0x22222222)};
+        void* r3{reinterpret_cast<void*>(0x33333333)};
+
+        void* r12{reinterpret_cast<void*>(0xCCCCCCCC)};
+        void* lr{nullptr};
+        void* pc{nullptr};
+        void* xpsr{reinterpret_cast<void*>(xpsrDefaultValue)};
     };
 
     struct StackFrame
@@ -39,6 +57,8 @@ namespace hal::kernel
         SoftwareStackFrame softwareStackFrame;
         ExceptionStackFrame exceptionStackFrame;
     };
+
+    void* InitializeStackFrame(void* const buffer, std::size_t size, void* obj);
 }
 
 #endif
