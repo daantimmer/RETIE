dea5e8851366e514ae9d7fca3fe2e83bf6fc7290
Daan Timmer <daan.timmer@gmail.com>
2022-07-07 00:30:58 +0200

    fixed startup.cpp

--- source/hal/cortex/Startup.cpp
+++ source/hal/cortex/Startup.cpp
@@ -12,7 +12,6 @@ extern "C"
     extern void SystemCoreClockUpdate();
     extern int main();
 
-    /*----------Symbols defined in linker script----------------------------------*/
     extern unsigned int _sidata;
     extern unsigned int _sdata;
     extern unsigned int _edata;
@@ -27,37 +26,68 @@ extern "C"
     extern std::uint32_t __processStack_start[];
     extern std::uint32_t __processStack_end[];
 
-    extern const char *__privelegedStack_end[];
+    extern const char* __privelegedStack_end[];
+}
 
-    [[noreturn]] void /*__attribute__((naked))*/ Startup()
+namespace
+{
+    extern "C"
     {
-        asm volatile("ldr r0, =__processStack_end");
-        asm volatile("msr psp, r0");
-        asm volatile("movs r0, %[consrolSpselMask]" ::[consrolSpselMask] "i"(CONTROL_SPSEL_Msk));
-        asm volatile("msr control, r0");
-        asm volatile("isb");
-
-        std::copy(&_sidata, &_sidata + (&_edata - &_sdata), &_sdata);
-
-        // std::copy(&_siccmram, &_siccmram + (&_eccmram - &_sccmram), &_sccmram);
-
-        std::fill(&_sbss, &_ebss, 0);
-
-        __libc_init_array();
-
-        SystemInit();
-
-        SystemCoreClockUpdate();
-
-#pragma GCC diagnostic push
-#pragma GCC diagnostic ignored "-Wpedantic"
-        main();
-#pragma GCC diagnostic pop
-
-        __libc_fini_array();
-
-        while (true)
+        void SetUp()
         {
+            std::copy(&_sidata, &_sidata + (&_edata - &_sdata), &_sdata);
+
+            // std::copy(&_siccmram, &_siccmram + (&_eccmram - &_sccmram), &_sccmram);
+
+            std::fill(&_sbss, &_ebss, 0);
+
+            __libc_init_array();
+
+            SystemInit();
+
+            SystemCoreClockUpdate();
         }
+
+        [[noreturn]] void TearDown()
+        {
+            __libc_fini_array();
+
+            while (true)
+            {}
+        }
+    }
+}
+
+extern "C"
+{
+    [[noreturn]] void __attribute__((naked)) Startup()
+    {
+        asm volatile(
+            R"(
+                ldr r0, =__processStack_end
+                msr psp, r0
+                movs r0, %[consrolSpselMask]
+                msr control, r0
+                isb
+            )"
+
+            R"(
+                ldr r0, = SetUp
+                blx r0
+            )"
+
+            R"(
+                ldr r0, = main
+                blx r0
+            )"
+
+            R"(
+                ldr r0, = TearDown
+                blx r0
+            )"
+
+            R"(
+                b .
+            )" ::[consrolSpselMask] "i"(CONTROL_SPSEL_Msk));
     }
 }
