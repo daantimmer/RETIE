1eef697e3eb8bec43a7e593e1cfe92590f575ad7
Daan Timmer <daan.timmer@gmail.com>
2022-07-12 21:49:39 +0200

    updated cubemx project

--- source/stm32g474/cubemx/Core/Inc/stm32g4xx_it.h
+++ source/stm32g474/cubemx/Core/Inc/stm32g4xx_it.h
@@ -46,16 +46,7 @@
 /* USER CODE END EM */
 
 /* Exported functions prototypes ---------------------------------------------*/
-void NMI_Handler(void);
-void HardFault_Handler(void);
-void MemManage_Handler(void);
-void BusFault_Handler(void);
-void UsageFault_Handler(void);
-void SVC_Handler(void);
-void DebugMon_Handler(void);
-void PendSV_Handler(void);
 void SysTick_Handler(void);
-void EXTI15_10_IRQHandler(void);
 /* USER CODE BEGIN EFP */
 
 /* USER CODE END EFP */
