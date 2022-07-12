83d76912dc1c57879c77e654026d2e660ff29ef7
Daan Timmer <daan.timmer@gmail.com>
2022-07-12 21:50:09 +0200

    updated cmake files to reflect library changes

--- source/stm32g474/main.cpp
+++ source/stm32g474/main.cpp
@@ -1,8 +1,25 @@
-#include "hal/stm/EventDispatcher.hpp"
+#include "hal/stm/SystemTick.hpp"
+#include "infra/timer/Timer.hpp"
+#include "infra/util/IntrusiveSet.hpp"
+#include "kernel/EventDispatcher.hpp"
+#include "kernel/IdleThread.hpp"
+#include "kernel/MainThread.hpp"
+#include "kernel/Scheduler.hpp"
+#include "kernel/StaticThread.hpp"
+#include "kernel/arch/RequestContextSwitch.hpp"
 
 extern "C" int RealMain()
 {
-    static hal::EventDispatcherStm eventDispatcher;
+    static hal::SystemTickStm systemTick;
+
+    static kernel::IdleThread idleThread{[]() {
+        while (true)
+        {
+            asm volatile("wfe");
+        }
+    }};
+
+    static kernel::EventDispatcher::WithSize<50> eventDispatcher;
 
     eventDispatcher.Run();
     return 0;
