9d7c79d076553cd000287e8758c34dd316bbfe56
Timmer, Daan <daan.timmer@philips.com>
2022-07-20 23:46:41 +0200

    Changed ThreadList from IntrusiveSet to IntrusivePriorityQueue

--- source/kernel/ThreadList.hpp
+++ source/kernel/ThreadList.hpp
@@ -1,14 +1,17 @@
 #ifndef KERNEL_THREADLIST_HPP
 #define KERNEL_THREADLIST_HPP
 
-#include "infra/util/IntrusiveSet.hpp"
+#include "assert.h"
+//
+#include "infra/util/IntrusivePriorityQueue.hpp"
 
 namespace kernel
 {
 
     struct ThreadControlBlockItem;
 
-    using ThreadList = infra::IntrusiveSet<ThreadControlBlockItem, std::greater<ThreadControlBlockItem>>;
+    using ThreadList = infra::IntrusivePriorityQueue<ThreadControlBlockItem>;
+    // using ThreadList = infra::IntrusiveSet<ThreadControlBlockItem, std::greater<ThreadControlBlockItem>>;
     using ThreadListNode = ThreadList::NodeType;
 }
 
